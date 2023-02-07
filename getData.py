6812dd8442d3dc0d15991ef95dba342229d71111
microslaw <milosz.grunwald@gmail.com>
2023-02-07 17:12:37 +0100

    added processed videos

--- getData.py
+++ getData.py
@@ -8,7 +8,7 @@ black = "#000000"
 white = "#ffffff"
 blue = "#0000a0"
 global mainWindow
-global imgHolder
+global imgHolder1
 
 delay = 10
 
@@ -26,20 +26,43 @@ def getRGBFrame():
     rgbFrame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     return rgbFrame
 
+def convertToTkinterImage(frame):
+    frame = Image.fromarray(frame)
+    photo = ImageTk.PhotoImage(image = frame)
+    return photo
     
-
 def updateUi():
     arrayFrame = getRGBFrame()
-    frame = Image.fromarray(arrayFrame)
-    photo = ImageTk.PhotoImage(image = frame)
-    imgHolder.configure(image = photo)
-    imgHolder.image = photo
+
+    images = dict()
+
+    images["normal"] = arrayFrame
+    images["treshold"] = tresholdImage(arrayFrame)
+    images["laplacian"] = laplacianImage(arrayFrame)
+    
+    for key, value in images.items():
+        images[key] = convertToTkinterImage(value)
+    
+    imgHolder1.configure(image = images["normal"])
+    imgHolder1.image = images["normal"]
+
+    imgHolder2.configure(image = images["treshold"])
+    imgHolder2.image = images["treshold"]
+
+    imgHolder3.configure(image = images["laplacian"])
+    imgHolder3.image = images["laplacian"]
+    
     mainWindow.after(delay, updateUi)
 
 cap = cv.VideoCapture(0)
 mainWindow = tk.Tk()
-imgHolder = tk.Label(mainWindow, width = 400, height = 400, bg=blue)
-imgHolder.grid(row = 0, column = 0)
+imgHolder1 = tk.Label(mainWindow, width = 400, height = 400, bg=blue)
+imgHolder1.grid(row = 0, column = 0)
+imgHolder2 = tk.Label(mainWindow, width = 400, height = 400, bg=blue)
+imgHolder2.grid(row = 0, column = 1)
+imgHolder3 = tk.Label(mainWindow, width = 400, height = 400, bg=blue)
+imgHolder3.grid(row = 1, column = 0)
+
 
 mainWindow.geometry("800x800")
 mainWindow.configure(bg=black)
--- imagePreprocessing.py
+++ imagePreprocessing.py
@@ -2,11 +2,24 @@ import cv2 as cv
 import numpy as np
 
 
-def preprocessImage(image):
+def preprocessImage(image, shrink=False):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray, (5, 5), 0)
     cv.imshow("blur", blur)
-    resized = cv.resize(blur, (32,32))
+    if shrink:
+        resized = cv.resize(blur, (32,32))
 
     return resized
+
+def getContours(image):
+    contours, hierarchy = cv.findContours(image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    return contours
+
+def tresholdImage(image):
+    treshold = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    return treshold
+
+def laplacianImage(image):
+    laplacian = cv.Laplacian(image, cv.CV_64F)
+    return laplacian
 
