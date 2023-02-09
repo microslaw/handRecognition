dd08c6f7d4790e51055b24370bce663d44675ee2
microslaw <milosz.grunwald@gmail.com>
2023-02-09 16:49:59 +0100

    updated ui, added skip option and class display

--- getData.py
+++ getData.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import tkinter as tk
+import random
 from imagePreprocessing import *
 from PIL import Image, ImageTk
 
@@ -30,8 +31,10 @@ def convertToTkinterImage(frame):
     frame = Image.fromarray(frame)
     photo = ImageTk.PhotoImage(image = frame)
     return photo
-    
-def updateUi():
+
+
+def updateFrameDisplay():
+    global imgHolder
     arrayFrame = getRGBFrame()
 
     images = dict()
@@ -42,41 +45,69 @@ def updateUi():
     
     for key, image in images.items():
         images[key] = convertToTkinterImage(image)
+        imgHolders[key].configure(image = images[key])
+        imgHolders[key].image = images[key]
     
-    imgHolder1.configure(image = images["normal"])
-    imgHolder1.image = images["normal"]
-
-    imgHolder2.configure(image = images["treshold"])
-    imgHolder2.image = images["treshold"]
-
-    imgHolder3.configure(image = images["simple"])
-    imgHolder3.image = images["simple"]
-    
-    mainWindow.after(delay, updateUi)
+    mainWindow.after(delay, updateFrameDisplay)
 
 def saveFrame():
     saveFrame.count += 1
     arrayFrame = getRGBFrame()
-    filepath = "frame%d.jpg" % (saveFrame.count)
+    filepath = "%s_%d.jpg" % (handShape, saveFrame.count)
     cv.imwrite(filepath, arrayFrame)
+    nextImage()
 saveFrame.count = 0
 
-cap = cv.VideoCapture(0)
-mainWindow = tk.Tk()
-imgHolder1 = tk.Label(mainWindow, width = videoResolution, height = videoResolution, bg=blue)
-imgHolder1.grid(row = 0, column = 0)
-imgHolder2 = tk.Label(mainWindow, width = videoResolution, height = videoResolution, bg=blue)
-imgHolder2.grid(row = 1, column = 0)
-imgHolder3 = tk.Label(mainWindow, width = videoResolution, height = videoResolution, bg=blue)
-imgHolder3.grid(row = 0, column = 1)
-saveButton = tk.Button(mainWindow, text = "Save", width = 50, height = 10, bg=blue, anchor="se", command = saveFrame)
-saveButton.grid(row = 1, column = 1)
+def getHandShape():
+    handShapes = ["rock", "paper", "scissors", "unknown"]
+    tmpHandShapes = handShapes.copy()
+    tmpHandShapes.remove(getHandShape.previous)
+    toReturn = random.choice(tmpHandShapes)
+    getHandShape.previous = toReturn
+    return toReturn
+getHandShape.previous = "unknown"
 
-resXres = "%sx%s" % (videoResolution*2, videoResolution*2)
-mainWindow.geometry(resXres)
-mainWindow.configure(bg=black)
+def nextImage():
+    global handShape, classLabel
+    handShape = getHandShape()
+    classLabel.configure(text = handShape)
 
+handShape = getHandShape()
 
-updateUi()
+def createUI():
+    global mainWindow, cap, imgHolders, saveButton, skipButton, classLabel, handShape
+
+    cap = cv.VideoCapture(0)
+    mainWindow = tk.Tk()
+
+    imgHolders = {"normal": None, "treshold": None, "simple": None}
+
+    imgHolders["normal"] = tk.Label(mainWindow, width = videoResolution, height = videoResolution, bg=black, anchor="se")
+    imgHolders["normal"].grid(row = 0, column = 0)
+    
+    imgHolders["treshold"] = tk.Label(mainWindow, width = videoResolution, height = videoResolution, bg=black, anchor="ne")
+    imgHolders["treshold"].grid(row = 1, column = 0, rowspan=4)
+    
+    imgHolders["simple"] = tk.Label(mainWindow, width = videoResolution, height = videoResolution, bg=black, anchor="sw")
+    imgHolders["simple"].grid(row = 0, column = 1, columnspan=2)
+
+    classLabel = tk.Label(mainWindow, text = handShape, width=10, height=3, bg=blue, fg=white, borderwidth=3, justify="center")
+    classLabel.grid(row = 1, column = 1)
+
+    countLabel = tk.Label(mainWindow, text = handShape, width=10, height=3, bg=blue, fg=white, borderwidth=3, justify="center")
+    countLabel.grid(row = 1, column = 2)
+
+    saveButton = tk.Button(mainWindow, text = "Save", width=10, height=3,  bg=blue, fg=white, borderwidth=3, justify="center", command = saveFrame)
+    saveButton.grid(row = 2, column = 1)
+
+    skipButton = tk.Button(mainWindow, text = "skip", width=10, height=3, bg=blue, fg=white, borderwidth=3, justify="center", command = nextImage)
+    skipButton.grid(row = 2, column = 2   )
+    
+    resXres = "%sx%s" % (videoResolution*2, videoResolution*2)
+    mainWindow.geometry(resXres)
+    mainWindow.configure(bg=black)
+
+createUI()
+updateFrameDisplay()
 mainWindow.mainloop()
 
