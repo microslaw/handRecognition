ddd2bb2261726b0f436692bc2fababaf7f649e72
microslaw <milosz.grunwald@gmail.com>
2023-02-18 12:53:01 +0100

    added basic model training pipeline

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import numpy as np
 from keras import *
 import tensorflow as tf
+
+from trainModel import *
+
+trainImages, trainLabels, testImages, testLabels = loadDataset()
+trainModel(trainImages, trainLabels)
--- trainModel.py
+++ trainModel.py
@@ -8,7 +8,7 @@ trainTestSplit = 0.8
 
 def createModel():
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape = (256, 256, 3)),
+        keras.layers.Flatten(input_shape = (32, 32)),
         keras.layers.Dense(128, activation = tf.nn.relu),
         keras.layers.Dense(4, activation = tf.nn.softmax)
     ])
@@ -17,18 +17,20 @@ def createModel():
 
 def trainModel(trainImages, trainLabels):
     model = createModel()
+    print(trainImages.shape, trainLabels.shape)
     model.fit(x = trainImages, y = trainLabels, epochs = 5, batch_size = 50)
     model.save("v1.h5")
     
 
 def loadDataset():
-    images = np.load("data/trainImages.npy")
-    labels = np.load("data/trainLabels.npy")
+    images = np.load("images.npy")
+    labels = np.load("labels.npy")
     trainImages = images[:int(len(images)*trainTestSplit)]
     trainLabels = labels[:int(len(labels)*trainTestSplit)]
     testImages = images[int(len(images)*trainTestSplit):]
     testLabels = labels[int(len(labels)*trainTestSplit):]
+    print("loaded")
+    print(trainImages.shape, trainLabels.shape)
+    print("size returned")
+    
     return trainImages, trainLabels, testImages, testLabels
-
-trainImages, trainLabels, testImages, testLabels = loadDataset()
-trainModel(trainImages, trainLabels)
